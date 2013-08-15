23b01ec291aea77ef0ec0c28ea3b1e4158b8882f
Simon Watkins <simonwatkins@Simons-MacBook-Air.local>
2013-08-16 00:54:46 +1000

    All working, test43 failing - Std output mismatch

--- thebox.c
+++ thebox.c
@@ -4,124 +4,76 @@
 typedef struct {
     int xPos;
     int yPos;
-} point;
+} Point;
+
+typedef struct {
+    int alph[26];
+} Alphabet;
 
 typedef struct {
     char** map;
     int xDim;
     int yDim;
     char direction;
-    int currentXPos;
-    int currentYPos;
-} mapInfo;
+    Point currentPos;
+} MapInfo;
 
-void printMap(mapInfo);
-mapInfo startSim(mapInfo);
-mapInfo checkSquare(mapInfo);
-mapInfo moveBall(mapInfo);
-point searchMap(mapInfo, char);
+/* Function prototypes */
+void print_map(MapInfo);
+MapInfo start_sim(MapInfo);
+MapInfo check_square(MapInfo);
+MapInfo move_ball(MapInfo);
+Point search_map(MapInfo, char);
+int check_letters(Alphabet);
+MapInfo read_file(MapInfo, char *);
+char** create_array(MapInfo);
+char** populate_array(MapInfo, char *);
+char reflector_nesw(char);
+char reflector_nwse(char);
+char reflector(char);
+Point launchpad(Point, char);
+int check_input(char, int, MapInfo);
 
 int main(int argc, char *argv[]) {
     int maxSteps;
-    mapInfo info;
+    MapInfo info;
+    int currentSteps = 0;
 
     /* Check for correct number of inputs */
     if(argc == 2) {
         maxSteps = 10;
     } else if(argc == 3) {
         maxSteps = atoi(argv[2]);
+        if(maxSteps>=1000 || maxSteps<=0) {
+            fprintf(stderr, "Bad max steps.\n");
+            return 2;
+        }
     } else {
-        fprintf(stderr, "Usage: thebox mapfile [maxsteps]");
+        fprintf(stderr, "Usage: thebox mapfile [maxsteps]\n");
         return 1;
     }
 
     /* Main game loop */
-    int playing = 1;
-    int first = 1;
     while(1) {
-
-        /* Open file and store map dimensions */
-        FILE *file;
-        file = fopen(argv[1], "r");
-        
-        if(file == 0) {
-            fprintf(stderr, "Missing map file.\n");
-            return 3;
-        } else {
-            fscanf(file, "%d %d", &info.xDim, &info.yDim);
-        }
-        
-        if(first) {
-            /* Create 2d array of chars */
-            int i;
-            info.map = (char**) malloc(info.xDim*sizeof(char*));
-            for(i=0; i<info.xDim; i++) {
-                info.map[i] = (char*) malloc(info.yDim*sizeof(char));
-            }
-            first = 0;
-        }
-       
-        /* Store chars to array */
-        int row = -1;
-        int column = 0;
-        char ch;
-		int rowCount = 0;
-		char nextLetter = '*';
-        while((ch=fgetc(file)) != EOF) {
-			if((ch>='A' && ch<='Z')) {
-				if(nextLetter=='*') {
-					nextLetter = ++ch;
-				} else {
-					if(ch!=nextLetter) {
-						fprintf(stderr, "Missing letters.\n");
-						return 7;
-					}
-				}
-			}
-            if(row != -1) {
-                if(ch == '\n') {
-                    row++;
-                    column = 0;
-					rowCount++;
-					if(rowCount>info.xDim) {
-						fprintf(stderr, "Map file is the wrong size.\n");
-						return 4;
-					}
-                } else {
-					if(!(ch=='.' || ch=='/' || ch=='\\' || ch=='@' || ch=='=' || (ch>='A' && ch<='Z'))) {
-						fprintf(stderr, "Bad map char.\n");
-						return 5;
-					}
-                    info.map[row][column] = ch;
-                    column++;
-					if(column > info.yDim) {
-						fprintf(stderr, "Map file is the wrong size.\n");
-						return 4;
-					}
-                }
-            } else {
-                row++;
-            }
-        }
-
-        printMap(info);
-        info = startSim(info);
-        printMap(info);
-        int currentSteps = 0;
-        while(playing) {
-            if(currentSteps>maxSteps) {
+        info = read_file(info, argv[1]);
+        print_map(info);
+        info = start_sim(info);
+        currentSteps = 0;
+        while(1) {
+            if(currentSteps>=maxSteps) {
+                printf("End of simulation.\n");
                 break;
             }
-            info = checkSquare(info);
-            info = moveBall(info);
+            if(info.currentPos.xPos < 0 || info.currentPos.xPos >= info.xDim 
+                || info.currentPos.yPos < 0 || 
+                info.currentPos.yPos >= info.yDim) {
+                printf("End of simulation.\n");
+                break;
+            }
+            info = check_square(info);
+            info = move_ball(info);
             currentSteps++;
-            printMap(info);
-            printf("info.xDim: %d\n", info.currentXPos); /* DEBUGGING!!!!!!!!!!! */
-            if(info.currentXPos < 0 || info.currentXPos >= info.xDim 
-                || info.currentYPos < 0 || info.currentYPos > info.yDim) {
-                break;
-            }
-            printf("CurrentXPos: %d\n", info.currentXPos); /* DEBUGGING!!!!!!!!!!! */
+            print_map(info);
         }
     }
 
@@ -129,35 +81,46 @@ int main(int argc, char *argv[]) {
 
 }
 
-void printMap(mapInfo info) {
+void print_map(MapInfo info) {
     /* Print the map to screen */ 
     int row1;
     int column1;
-    for(row1=0; row1<info.yDim; row1++) {
-        for(column1=0; column1<info.xDim; column1++ ) {
+    for(row1 = 0; row1<info.xDim; row1++) {
+        for(column1 = 0; column1<info.yDim; column1++) {
             printf("%c", info.map[row1][column1]);
         }
-    printf("%c", '\n');
+        printf("%c", '\n');
     }
     printf("%c", '\n');
     
 }
 
-mapInfo startSim(mapInfo info) {
+MapInfo start_sim(MapInfo info) {
     /* Take input where to start */
-    char side;
-    int sidePos;
-    char leftOver;
-    printf("(Side Pos)> ");
-    scanf("%c%d%c",&side,&sidePos, &leftOver);
+    char side, buffer[7];
+    int sidePos, startRow, startCol;
+    printf("(side pos)>");
+    fgets(buffer, 7, stdin);
+    sscanf(buffer, "%c%d", &side, &sidePos);
+
+    if(feof(stdin)) {
+        exit(0);
+    }
+
+    if(side == '\n') {
+        info = start_sim(info);
+        return info;
+    }
+
+    if(!check_input(side, sidePos, info)) {
+        info = start_sim(info);
+        return info;
+    }
 
     /* Take one from sidepos as array starts at 0 */
     sidePos--;
 
     /* Drop ball in at startPos */
-    int startRow;
-    int startCol;
-	
     switch(side) {
         case 'N':
             startRow = 0;
@@ -166,15 +129,13 @@ mapInfo startSim(mapInfo info) {
             break;
 
         case 'E':
-			printf("StartSim xDim: %d\n", info.xDim);
-            startCol = info.xDim-1;
+            startCol = info.yDim-1;
             startRow = sidePos;
             info.direction = 'W';
-			printf("Intended Current Pos(startSim): %d, %d\n", startRow, startCol);
             break;
 
         case 'S':
-            startRow = info.yDim-1;
+            startRow = info.xDim-1;
             startCol = sidePos;
             info.direction = 'N';
             break;
@@ -185,35 +146,33 @@ mapInfo startSim(mapInfo info) {
             info.direction = 'E';
             break;
 
-        default:;
+        default:            ;
 
     }
-    info.currentXPos = startRow;
-    info.currentYPos = startCol;
-	printf("Current Pos(startSim): %d, %d\n", info.currentXPos, info.currentYPos);
+
+    info.currentPos.xPos = startRow;
+    info.currentPos.yPos = startCol;
     return info;
 }
 
-mapInfo checkSquare(mapInfo info) {
-    int currentX = info.currentXPos;
-    int currentY = info.currentYPos;
+MapInfo check_square(MapInfo info) {
+    int currentX = info.currentPos.xPos;
+    int currentY = info.currentPos.yPos;
     char square = info.map[currentX][currentY];
-    point newPoint;
-	
+    Point newPoint;
 
     if(square >= '1' && square < '9') {
         info.map[currentX][currentY] = ++square;
     } else if(square >= 'A' && square <= 'Z') {
-        newPoint = searchMap(info, ++square);
-        printf("New Point: %d, %d\n", newPoint.xPos, newPoint.yPos);
+        newPoint = search_map(info, ++square);
         if(newPoint.xPos == -1) {  /* Next char isn't found on map */
-            newPoint = searchMap(info, 'A');
-            info.currentXPos = newPoint.xPos;
-            info.currentYPos = newPoint.yPos;
+            newPoint = search_map(info, 'A');
+            info.currentPos.xPos = newPoint.xPos;
+            info.currentPos.yPos = newPoint.yPos;
             return info;
         } else { /* Next char is found */
-            info.currentXPos = newPoint.xPos;
-            info.currentYPos = newPoint.yPos;
+            info.currentPos.xPos = newPoint.xPos;
+            info.currentPos.yPos = newPoint.yPos;
             return info;
         }
     } else {
@@ -223,103 +182,51 @@ mapInfo checkSquare(mapInfo info) {
                 break;
 
             case '/':
-                switch(info.direction) {
-                    case 'N':
-                        info.direction = 'E';
-                        break;
-                    case 'E':
-                        info.direction = 'N';
-                        break;
-                    case 'S':
-                        info.direction = 'W';
-                        break;
-                    case 'W':
-                        info.direction = 'S';
-                        break;
-                }
+                info.direction = reflector_nesw(info.direction);
                 break;
 
             case '\\':
-                switch(info.direction) {
-                    case 'N':
-                        info.direction = 'W';
-                        break;
-                    case 'E':
-                        info.direction = 'S';
-                        break;
-                    case 'S':
-                        info.direction = 'E';
-                        break;
-                    case 'W':
-                        info.direction = 'N';
-                        break;
-                }
+                info.direction = reflector_nwse(info.direction);
                 break;
 
             case '=':
-                switch(info.direction) {
-                    case 'N':
-                        info.direction = 'S';
-                        break;
-                    case 'E':
-                        info.direction = 'W';
-                        break;
-                    case 'S':
-                        info.direction = 'N';
-                        break;
-                    case 'W':
-                        info.direction = 'E';
-                        break;
-                }
+                info.direction = reflector(info.direction);
                 break;
 
             case '@':
-                switch(info.direction) {
-                    case 'N':
-                        info.currentXPos -= 4;
-                        break;
-                    case 'E':
-                        info.currentYPos += 4;
-                        break;
-                    case 'S':
-                        info.currentXPos += 4;
-                        break;
-                    case 'W':
-                        info.currentYPos -= 4;
-                        break;
-                }
+                info.currentPos = launchpad(info.currentPos, info.direction);
                 break;
         }
     }
     return info;
 }
 
-mapInfo moveBall(mapInfo info) {
+MapInfo move_ball(MapInfo info) {
     switch(info.direction) {
         case 'N':
-            info.currentXPos--;
+            info.currentPos.xPos--;
             break;
         case 'E':
-            info.currentYPos++;
+            info.currentPos.yPos++;
             break;
         case 'S':
-            info.currentXPos++;
+            info.currentPos.xPos++;
             break;
         case 'W':
-            info.currentYPos--;
+            info.currentPos.yPos--;
             break;
     }
     return info;
 }
 
-point searchMap(mapInfo info, char searchChar) {
+Point search_map(MapInfo info, char searchChar) {
     int i;
     int j;
     char onSquare;
-    point newPoint;
+    Point newPoint;
 
-    for(i=0; i<info.xDim; i++) {
-        for(j=0; j<info.yDim; j++) {
+    for(i = 0; i<info.xDim; i++) {
+        for(j = 0; j<info.yDim; j++) {
             onSquare = info.map[i][j];
             if((onSquare == searchChar)) {
                 newPoint.xPos = i;
@@ -331,4 +238,230 @@ point searchMap(mapInfo info, char searchChar) {
     newPoint.xPos = -1;
     newPoint.yPos = -1;
     return newPoint;
+}
+
+MapInfo read_file(MapInfo info, char *filename) {
+    /* Open file and store map dimensions */
+    FILE *file;
+    int testVar;
+    char middleChar;
+
+    file = fopen(filename, "r");
+    
+    if(file == 0) {
+        fprintf(stderr, "Missing map file.\n");
+        exit(3);
+    } else {
+        testVar = fscanf(file, "%d%c%d", &info.xDim, &middleChar, &info.yDim);
+        if(testVar != 3 || middleChar != ' ') {
+            fprintf(stderr, "Bad map dimensions.\n");
+            exit(4);
+        }
+        if(info.xDim <= 0 || info.yDim <= 0) {
+            fprintf(stderr, "Bad map dimensions.\n");
+            exit(4);
+        }
+    }
+
+    info.map = create_array(info);
+    
+    info.map = populate_array(info, filename);
+   
+    
+
+    return info;
+}
+
+char** create_array(MapInfo info) {
+    int first = 1;
+    if(first) {
+        /* Create 2d array of chars */
+        int i;
+        info.map = (char**) malloc(info.xDim*sizeof(char*));
+        for(i = 0; i<info.xDim; i++) {
+            info.map[i] = (char*) malloc(info.yDim*sizeof(char));
+        }
+        first = 0;
+    }
+
+    return info.map;
+}
+
+char** populate_array(MapInfo info, char *filename) {
+    /* Store chars to array */
+    FILE *file;
+    int row = -1, column = 0, rowCount = 0, j, firstLinePassed = 0;
+    char ch;
+    Alphabet alph;
+    for(j = 0; j<26; j++) {
+        alph.alph[j] = 0;
+    }
+    file = fopen(filename, "r");
+    while((ch = fgetc(file)) != EOF) {
+        if(!firstLinePassed && ch!='\n') {
+            continue;
+        } else {
+            firstLinePassed = 1;
+        }
+        if((ch>='A' && ch<='Z')) { /* Check if a letter */
+            if (alph.alph[ch-65]) {
+                fprintf(stderr, "Missing letters.\n");
+                exit(7);
+            } else {
+                alph.alph[ch-65] = 1;
+            }
+        }
+        if(row != -1) {
+            if(ch == '\n') {
+                row++;
+                rowCount++;
+                if(row>info.xDim || column!=info.yDim) {
+                    fprintf(stderr, "Map file is the wrong size.\n");
+                    exit(6);
+                }
+                column = 0;
+            } else {
+                if(!(ch=='.' || ch=='/' || ch=='\\' || ch=='@' || ch=='=' || 
+                    (ch>='A' && ch<='Z'))) {
+                    fprintf(stderr, "Bad map char.\n");
+                    exit(5);
+                }
+                info.map[row][column] = ch;
+                column++;
+                if(column > info.yDim) {
+                    fprintf(stderr, "Map file is the wrong size.\n");
+                    exit(6);
+                }
+            }
+        } else {
+            row++;
+        }
+    }
+    if(!check_letters(alph)) {
+        fprintf(stderr, "Missing letters.\n");
+        exit(7);
+    }
+    /* Check map ended the right size */
+    if(row!=info.xDim) {
+        fprintf(stderr, "Map file is the wrong size.\n");
+        exit(6);
+    }
+    return info.map;
+}
+
+/* Returns 1 if all letters valid, 0 otherwise */
+int check_letters(Alphabet alph) { 
+    int noEarlyLetter = 0;
+    int i;
+    int letterCount = 0;
+
+    for(i = 0; i<26; i++) {
+        if(alph.alph[i]) {
+            letterCount++;
+        }
+        if(!alph.alph[i] && !noEarlyLetter) {
+            noEarlyLetter = 1;
+        }
+        if(alph.alph[i] && noEarlyLetter) {
+            return 0;
+        }
+    }
+    if(!letterCount || letterCount>1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+char reflector_nesw(char direction) {
+    char newDirection;
+    switch(direction) {
+        case 'N':
+            newDirection = 'E';
+            break;
+        case 'E':
+            newDirection = 'N';
+            break;
+        case 'S':
+            newDirection = 'W';
+            break;
+        case 'W':
+            newDirection = 'S';
+            break;
+    }
+    return newDirection;
+}
+
+char reflector_nwse(char direction) {
+    char newDirection;
+    switch(direction) {
+        case 'N':
+            newDirection = 'W';
+            break;
+        case 'E':
+            newDirection = 'S';
+            break;
+        case 'S':
+            newDirection = 'E';
+            break;
+        case 'W':
+            newDirection = 'N';
+            break;
+    }
+    return newDirection;
+}
+
+char reflector(char direction) {
+    char newDirection;
+    switch(direction) {
+        case 'N':
+            newDirection = 'S';
+            break;
+        case 'E':
+            newDirection = 'W';
+            break;
+        case 'S':
+            newDirection = 'N';
+            break;
+        case 'W':
+            newDirection = 'E';
+            break;
+    }
+    return newDirection;
+}
+
+Point launchpad(Point currentPos, char direction) {
+    Point newPos = currentPos;
+    switch(direction) {
+        case 'N':
+            newPos.xPos -= 4;
+            break;
+        case 'E':
+            newPos.yPos += 4;
+            break;
+        case 'S':
+            newPos.xPos += 4;
+            break;
+        case 'W':
+            newPos.yPos -= 4;
+            break;
+    }
+    return newPos;
+}
+
+int check_input(char side, int sidePos, MapInfo info) {
+    if(!(side == 'N' || side == 'E' || side == 'S' || side == 'W') || 
+        sidePos <= 0) {
+        return 0;
+    }
+
+    if((side == 'N' || side == 'S') && (sidePos > info.xDim)) {
+        return 0;
+    }
+
+    if((side == 'E' || side == 'W') && (sidePos > info.yDim)) {
+        return 0;
+    }
+
+    return 1;
 }
